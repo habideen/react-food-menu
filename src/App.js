180954b7380a0bcf257ff6ed3234e4e1ebdbcab1
Habideen Ibrahim <habideenibrahim@gmail.com>
2021-06-29 04:39:18 +0100

    Restore crashed files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Navbar, NavbarBrand} from 'reactstrap';
 import { Component } from 'react';
 import Main from './components/MainComponent';
+//import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 /*import { DISHES } from './shared/dishes';*/
 
