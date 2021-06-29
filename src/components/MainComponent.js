45f6182491c5074f42430024a69d4c8f99501ace
Habideen Ibrahim <habideenibrahim@gmail.com>
2021-06-29 08:14:06 +0100

    Header and Footer component with fontawesome.css and bootstrap-social.css

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
-import {Navbar, NavbarBrand} from 'reactstrap';
 import { Component } from 'react';
 import Menu from './MenuComponent';
 import DishDetail from './MenuDetails';
+import Header from './HeaderComponent'
+import Footer from './FooterComponent'
 import { DISHES } from '../shared/dishes';
 
 
@@ -25,17 +26,16 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href='/'>Bravytech Brand</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
+
         <Menu dishes={this.state.dishes}
             onClick={(dishId) => this.onDishSelect(dishId)}/>
         <div className="container mt-4">
             <DishDetail mem={this.state.selectedDish}
                 dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         </div>
+
+        <Footer />
       </div>
     );
   }
